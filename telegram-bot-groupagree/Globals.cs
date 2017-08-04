8e829afd065507331bdbe37c7b6dce2a888532ea
Jan Braun <browny@wjclub.tk>
2017-08-04 14:21:44 +0200

    Added dummy file

---
+++ telegram-bot-groupagree/Globals.cs
@@ -0,0 +1,28 @@
+namespace telegrambotgroupagree {
+	public static class Globals {
+    //We have three configurations to compile the bot: As local test, nightly or release
+    //To test the bot locally you need to provide the 
+		public static string Botname =
+    //Use only the name prefix, for example groupagree instead of groupagreebot
+    //Put your bot's names in here
+		#if DEBUG
+			""
+		#elif NIGHTLY
+			""
+		#elif RELEASE
+			""
+    #endif
+      ;
+
+		public static string Apikey = 
+    //Add your API-Keys here
+			#if DEBUG
+			""
+			#elif NIGHTLY
+			""
+			#elif RELEASE
+			""
+			#endif
+			;
+		}
+}
