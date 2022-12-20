fdc570d46562e5b52fd754f338201ce938d7821a
Jan Braun <browny@wjclub.de>
2022-12-20 23:19:34 +0100

    style: :construction: started work on console runner

---
+++ runners/console/Program.cs
@@ -0,0 +1,28 @@
+﻿// using GroupAgreeBot.Services;
+// using Telegram.Bot;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Logging;
+// using Microsoft.Extensions.Hosting;
+
+// var host = Host.CreateDefaultBuilder(args);
+
+// var services = host.ConfigureServices((context, services) => {
+//     services.AddHttpClient("telegram_bot_client")
+//             .AddTypedClient<ITelegramBotClient>((httpClient, services) =>
+//             {
+//                 TelegramBotClientOptions options = new("599164885:AAFeuIhKvahJWpMiGrTOaYdnjYo4o1qErkg");
+//                 return new TelegramBotClient(options, httpClient);
+//             });
+
+//     // add logging
+//     services.AddLogging(configure => configure.AddConsole());
+
+//     // add storage
+//     services.AddSingleton<IGroupAgreeBotStorage, MockupStorage>();
+
+//     services.AddTransient<TelegramUpdateHandler>();
+
+//     services.AddHostedService<RequestHandler>();
+// });
+
+// await host.RunConsoleAsync();
